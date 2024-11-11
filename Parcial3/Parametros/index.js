bd096cc06c6dcf0187f9ddd589f4082248420299
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2024-11-10 19:01:33 -0600

    Parametros

---
+++ Parcial3/Parametros/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.text());
+
+app.get('/administrativos', (req, res) => {
+    console.log(req.query);
+    res.send('Servidor contestando a petición GET con Query');
+});
+
+app.post('/admin', (req, res) => {
+    console.log(req.body);
+    res.send('Servidor contestando a petición POST');
+});
+
+app.get('/estudiantes/:carrera', (req, res) => {
+    console.log(req.params.carrera);
+    console.log(req.query.control);
+    res.send('Servidor contestando a petición GET');
+});
+
+app.listen(8080, () => {
+    console.log(`Servidor express escuchando en http://localhost:8080`);
+});
+
