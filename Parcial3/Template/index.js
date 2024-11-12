f47d8f246841e3f9a37858b64f1df3149b31dafe
Leysly-Mendoza <leyslyymendozaf@gmail.com>
2024-11-11 20:40:05 -0600

    Template

--- Parcial3/Template/index.js
+++ Parcial3/Template/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-//const pug = require('pug');
 
 app.use(express.json());
 app.use(express.text());
-
-//console.log(__dirname);
-//console.log(__filename); 
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'))
@@ -15,19 +11,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/administrativos', (req, res) => {
     console.log(req.query);
-    //res.send('Servidor contestando a petición GET con Query');
     res.render('admin')
 });
 //Body
 app.post('/maestros', (req, res) => {
     console.log(req.body);
-    res.send('Servidor contestando a petición POST');
-});
-//Params
-app.get('/estudiantes/:carrera', (req, res) => {
-    console.log(req.params.carrera);
-    console.log(req.query.control);
-    res.send('Servidor contestando a petición GET');
+    res.render('admin')
 });
 
 app.listen(8080, () => {
